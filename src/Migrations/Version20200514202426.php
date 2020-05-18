30dd30845e8d83a229d4a8b648056341573ebe52
Naikok <sergiogayarre@gmail.com>
2020-05-18 14:15:23 +0200

    Update Version20200514202426.php

--- src/Migrations/Version20200514202426.php
+++ src/Migrations/Version20200514202426.php
@@ -24,7 +24,6 @@ final class Version20200514202426 extends AbstractMigration
 
         $this->addSql('CREATE TABLE activity (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(64) NOT NULL, start_date DATETIME NOT NULL, price DOUBLE PRECISION NOT NULL, end_date DATETIME NOT NULL, popularity INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, id_activity INT NOT NULL, people INT NOT NULL, created_at DATETIME NOT NULL, activity_date DATETIME NOT NULL, total_price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE booking ADD FOREIGN KEY (id_activity) REFERENCES activity (id)');
     }
 
     public function down(Schema $schema) : void
